23ec24e41dbdf966b1810ae7fecf920eb36245f6
Mohalshaiji <111268081+Mohalshaiji@users.noreply.github.com>
2025-03-16 23:14:54 -0700

    Update gui.c
    Add simple field for entry of name of mem file

--- src/gui.c
+++ src/gui.c
@@ -11,7 +11,19 @@ int main(int argc, char *argv[]) {
 
     // Make sure window exits when users exit the app
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    
+    //Create a grid layout and add it to the window
+    GtkWidget *grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
+    //Create a label for “Memory File”
+    GtkWidget *label_mem = gtk_label_new("Memory File:");
+    gtk_grid_attach(GTK_GRID(grid),label_mem,0, 0, 1, 1);  // column=0, row=0, width=1, height=1
 
+    //Create a text entry field with a default value
+    GtkWidget *entry_mem = gtk_entry_new();
+    gtk_entry_set_text(GTK_ENTRY(entry_mem), "program.mem");
+    gtk_grid_attach(GTK_GRID(grid),entry_mem,1, 0, 1, 1);  // column=1, row=0, width=1, height=1
+    
     //Display window
     gtk_widget_show_all(window);
     gtk_main();
